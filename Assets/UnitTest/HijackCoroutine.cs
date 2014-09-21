4e2dafa320b101e1c1681a53b6bf9ea075901824
Luckey_Sun <sunlaibing88@gmail.com>
2014-09-21 18:51:56 +0800

    添加 UnityTestTools

---
+++ Assets/UnitTest/HijackCoroutine.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(GUIText))]
+public class HijackCoroutine : MonoBehaviour {
+    // This will hold the counting up coroutine
+    IEnumerator _countUp;
+    IEnumerator _countDown;
+
+    IEnumerator _current;
+
+    int value = 0;
+
+	// Use this for initialization
+	void Start () {
+        _countUp = CountUp();
+        _countDown = CountDown();
+        StartCoroutine(DoHijack());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        guiText.text = value.ToString();
+	}
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Swith functions"))
+        {
+            if (_current == _countUp)
+                _current = _countDown;
+            else
+                _current = _countUp;
+        }
+
+        GUILayout.Label(value.ToString());
+    }
+
+    IEnumerator DoHijack()
+    {
+        while (true)
+        {
+            if (_current != null && _current.MoveNext())
+            {
+                yield return _current.Current;
+            }
+            else
+                yield return null;
+        }
+    
+    }
+
+    IEnumerator CountUp()
+    {
+        float increment = 0;
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.Q))
+                break;
+            increment += Time.deltaTime;
+            value += Mathf.RoundToInt(increment);
+            yield return null;
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        float increment = 0f;
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.Q))
+                break;
+            increment += Time.deltaTime;
+            value -= Mathf.RoundToInt(increment);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ Assets/UnitTest/TestCoroutine.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TestCoroutine : MonoBehaviour {
+
+    private bool isStartCall = false;
+    private bool isUpdateCall = false;
+    private bool isLateUpdateCall = false;
+
+	// Use this for initialization
+	void Start () {
+        if (!isStartCall)
+        {
+            Debug.Log("Start Call Begin.");
+            StartCoroutine(StartCountine());
+            Debug.Log("Start Call End");
+            isStartCall = true;
+        }
+	}
+
+    IEnumerator StartCountine()
+    {
+        Debug.Log("This is Start Coroutine");
+        yield return null;// new WaitForSeconds(1);
+        Debug.Log("This is Start Coroutine Call After.");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isUpdateCall)
+        {
+            Debug.Log("Update Call Begin.");
+            StartCoroutine(UpdateCoutine());
+            Debug.Log("Update Call End");
+            isUpdateCall = true;
+
+            //this.enabled = false;
+            this.gameObject.SetActive(false);
+        }
+	}
+
+    IEnumerator UpdateCoutine()
+    {
+        Debug.Log("This is Update Coroutine Call Before");
+        yield return null;// new WaitForSeconds(1);
+        Debug.Log("This is Update Coroutine Call After");
+    }
+
+    void LateUpdate()
+    {
+        if (!isLateUpdateCall)
+        {
+            Debug.Log("LateUpdate Call Begin");
+            StartCoroutine(LateCountine());
+            Debug.Log("LateUpdate Call End");
+            isLateUpdateCall = true;
+        }
+    }
+
+    IEnumerator LateCountine()
+    {
+        Debug.Log("This is Late Coroutine Call Before");
+        yield return null;// new WaitForSeconds(1);
+        Debug.Log("This is Late Coroutine Call After");
+    }
+}
